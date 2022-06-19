af73b46331909cbbf216bd7020cd956464cd6e72
HabipSanli <78694716+HabipSanli@users.noreply.github.com>
2022-06-19 18:48:36 +0300

    Revert "CRUD init"
    This reverts commit 1e0c95a0fd427756c59570505b829c370d9137d4.

--- Backend/PC-Part-Picker/Controllers/WeatherForecastController.cs
+++ Backend/PC-Part-Picker/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace PC_Part_Picker.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+    };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
