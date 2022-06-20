2b5af9d81a5b1ad74aef4d04f1b52e9e43c3cae2
batuhankutlu <52199113+batuhankutlu@users.noreply.github.com>
2022-06-20 23:55:12 +0300

    User CRUD init
    Co-Authored-By: Hasan Berkan Eroğlu <72980770+HBE18@users.noreply.github.com>
    Co-Authored-By: HabipSanli <78694716+HabipSanli@users.noreply.github.com>
    Co-Authored-By: Cathessis <44638729+Cathessis@users.noreply.github.com>

--- Backend/PC-Part-Picker/Controllers/UserController.cs
+++ Backend/PC-Part-Picker/Controllers/UserController.cs
@@ -1,32 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using PC_Part_Picker.Models;
+using System.Data;
 
 namespace PC_Part_Picker.Controllers
 {
     [ApiController]
-    [Route("users")]
+    [Route("user")]
     public class UserController : Controller
     {
-        public class User
-        {
-            public string username { get; set; }
-            public string password { get; set; }
-        }
 
         private readonly ILogger<UserController> _logger;
+        private readonly SqlConnection _connection;
+        private USER user = new USER
+        {
+            User_ID = Guid.NewGuid(),
+            email = "NULL",
+            password = "NULL",
+        };
 
         public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
+            string connectionString = "Data Source=localhost;Initial Catalog=P3_DB;User ID=ppp_user;Password=123456;Encrypt=False;";
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
         }
 
-        [HttpPost]
-        public string login_user(User us) 
+        [EnableCors("cors_allow")]
+        [HttpGet]
+        public ActionResult GetUser(string email, string password)
         {
-            string un = us.username;
-            string pw = us.password;
+            string query = "SELECT * FROM USERS WHERE email=" + email;
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    string sys_password = reader.GetString("password");
+                    if(sys_password.Equals(password))
+                    {
+                        user = new USER
+                        {
+                            User_ID = reader.GetGuid("User_ID"),
+                            email = reader.GetString("email"),
+                            password = reader.GetString("password")
+                        };
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest("Password not matched");
+                    }
+                }
+            }
+            return BadRequest("No User");
+        }
 
-            return un + "____" + pw;
+        [EnableCors("cors_allow")]
+        [HttpGet("auth")]
+        public ActionResult GetAuth()
+        {
+            if(!user.email.Equals("NULL") && !user.password.Equals("NULL"))
+            {
+                return Ok("User authenticated already");
+            }
+            else
+            {
+                return BadRequest("User not authenticated");
+            }
+        }
+
+        [EnableCors("cors_allow")]
+        [HttpPost]
+        public ActionResult PostUser(string email, string password)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "INSERT INTO USERS (User_ID, email, password) VALUES (@guid, @email, @password)";
+            command.Parameters.AddWithValue("@guid", Guid.NewGuid());
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@password", password);
+            try
+            {
+                int recordsAffected = command.ExecuteNonQuery();
+                return Ok(recordsAffected);
+            }
+            catch (SqlException e)
+            {
+                return BadRequest(e.Message);
+            }
+
         }
     }
 }
--- Backend/PC-Part-Picker/Models/USER.cs
+++ Backend/PC-Part-Picker/Models/USER.cs
@@ -1,9 +1,8 @@
 ﻿namespace PC_Part_Picker.Models;
 
-public class USERS
+public class USER
 {
 	public Guid User_ID { get; set; }
-	public string Username { get; set; }
 	public string email { get; set; }
 	public string password { get; set; }
 }
